c2031c8207d667b14cab4ae07494720b8ca15701
bernat alcudia <bernatalcudia@gmail.com>
2025-05-20 10:35:56 +0200

    feat(api):send message client to server

--- index.js
+++ index.js
@@ -6,4 +6,9 @@ io.on('connection', (socket) => {
     console.log('connection received')
     console.log(socket.id)
     socket.emit('hi', 'world')
+
+    socket.on('message', (arg) => {
+        console.log(arg)
+        socket.emit('response', 'with oreo and cheesecake')
+    })
 })
